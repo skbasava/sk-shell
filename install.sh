4accc5d205ce56bd8e1e0b26351ffcceb895627a
satish <satish.aradhya@gmail.com>
2026-02-04 19:28:53 +0530

    Upgraded URL in install

--- install.sh
+++ install.sh
@@ -10,12 +10,12 @@ echo "Installing sk..."
 # Check if running as root for /usr/local/bin
 if [ "$INSTALL_DIR" = "/usr/local/bin" ] && [ "$(id -u)" -ne 0 ]; then
     echo "This script requires sudo to install to $INSTALL_DIR"
-    echo "Please run: curl -sSL https://raw.githubusercontent.com/arpitbbhayani/x/master/install.sh | sudo sh"
+    echo "Please run: curl -sSL https://raw.githubusercontent.com/skbasava/sk-shell/main/install.sh | sudo sh"
     exit 1
 fi
 
 # Download the script
-curl -sSL https://raw.githubusercontent.com/arpitbbhayani/x/master/x -o "$INSTALL_DIR/$SCRIPT_NAME"
+curl -sSL https://raw.githubusercontent.com/skbasava/sk-shell/main/sk.sh -o "$INSTALL_DIR/$SCRIPT_NAME"
 
 # Make it executable
 chmod +x "$INSTALL_DIR/$SCRIPT_NAME"
